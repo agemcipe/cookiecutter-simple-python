4eeefcd1b7cb369343eec157bc91d8b547231130
agemcipe <jonathan.haas.ger@gmail.com>
2021-03-04 23:53:27 +0000

    Add git repo creation

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,15 @@
+if "{{cookiecutter.create_remote}}" == "yes":
+
+    import subprocess
+
+    print("--- Creating git repository ---")
+    subprocess.call(
+        ["git", "init", "-b", "main"]
+    )  # make sure you have git version > 2.28.0 , see https://stackoverflow.com/questions/42871542/how-can-i-create-a-git-repository-with-the-default-branch-name-other-than-maste
+    subprocess.call(["git", "add", "."])
+    subprocess.call(["git", "commit", "-m", "Initial commit"])
+
+    subprocess.call(["gh", "repo", "create"])
+
+    subprocess.call(["git", "remote", "-v"])
+    subprocess.call(["git", "push", "--set-upstream", "origin", "main"])
